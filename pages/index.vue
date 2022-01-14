4d1338befb0a5b37629cf2674ee6178a378b3aba
realabdullah <abdulodesanmi@gmail.com>
2022-01-14 20:47:45 +0100

    methods

--- pages/index.vue
+++ pages/index.vue
@@ -21,33 +21,33 @@
         </button>
       </form>
       <div class="taskss">
-        <div class="completed-tasks">
-          <div class="uncompleted-header">
-            <p>Fresh Tasks</p>
-          </div>
-          <div class="search search-animation">
-            <input type="text" placeholder="Search tasks here...">
-            <img src="../assets/search.png" alt="search">
-          </div>
-          <ul id="tasks" class="tasks">
-            <li class="element-animation" v-for="(todo, index) in todos" :key="todo.id">
-              <p>{{ todo.content }}</p>
-              <img @click="taskCompleted(index)" src="../assets/uncomplete.png" alt="uncomplete">
-            </li>
-          </ul>
-        </div>
+<!--        <div class="completed-tasks">-->
+<!--          <div class="uncompleted-header">-->
+<!--            <p>Fresh Tasks</p>-->
+<!--          </div>-->
+<!--          <div class="search search-animation">-->
+<!--            <input type="text" placeholder="Search tasks here...">-->
+<!--            <img src="../assets/search.png" alt="search">-->
+<!--          </div>-->
+<!--          <ul id="tasks" class="tasks">-->
+<!--            <li class="element-animation" v-for="(todo, index) in todos" :key="todo.id">-->
+<!--              <p>{{ todo.content }}</p>-->
+<!--              <img @click="taskCompleted(index)" src="../assets/uncomplete.png" alt="uncomplete">-->
+<!--            </li>-->
+<!--          </ul>-->
+<!--        </div>-->
 
-        <div class="completed-tasks">
-          <div class="completed-header">
-            <p>Completed Tasks</p>
-          </div>
-          <ul id="tasks" class="tasks">
-            <li class="completed-animation" v-for="(completedTodo, index) in completedTodos" :key="completedTodo.id">
-              <p>{{ completedTodo.content }}</p>
-              <img @click="deleteTask(index)" src="../assets/complete.png" alt="uncomplete">
-            </li>
-          </ul>
-        </div>
+<!--        <div class="completed-tasks">-->
+<!--          <div class="completed-header">-->
+<!--            <p>Completed Tasks</p>-->
+<!--          </div>-->
+<!--          <ul id="tasks" class="tasks">-->
+<!--            <li class="completed-animation" v-for="(completedTodo, index) in completedTodos" :key="completedTodo.id">-->
+<!--              <p>{{ completedTodo.content }}</p>-->
+<!--              <img @click="deleteTask(index)" src="../assets/complete.png" alt="uncomplete">-->
+<!--            </li>-->
+<!--          </ul>-->
+<!--        </div>-->
       </div>
     </div>
   </div>
@@ -55,6 +55,19 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = ''
+      }
+    }
+  }
 }
 </script>
