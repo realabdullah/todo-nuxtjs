024272dbc7f9f64f15a0e6d7030f8e91d12956a8
realabdullah <abdulodesanmi@gmail.com>
2022-01-14 20:36:45 +0100

    html

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,53 @@
 <template>
-  <div>Hello World</div>
+  <div class="doit">
+    <div class="header">
+      <h2>CHECK-IT</h2>
+    </div>
+
+    <div class="todo-body">
+      <form @submit.prevent="addNewTodo">
+        <div class="addtask-header">
+          <p>Add A New Task!</p>
+        </div>
+
+        <input v-model="newTask" name="newTodo" class="task-input" type="text" placeholder="Task Title..">
+
+        <button @click.prevent="addNewTodo" class="add">
+          <img src="../assets/add.png" alt="Add">
+          <p>Add New Task</p>
+        </button>
+      </form>
+      <div class="taskss">
+        <div class="completed-tasks">
+          <div class="uncompleted-header">
+            <p>Fresh Tasks</p>
+          </div>
+          <div class="search search-animation">
+            <input type="text" placeholder="Search tasks here...">
+            <img src="../assets/search.png" alt="search">
+          </div>
+          <ul id="tasks" class="tasks">
+            <li class="element-animation" v-for="(todo, index) in todos" :key="todo.id">
+              <p>{{ todo.content }}</p>
+              <img @click="taskCompleted(index)" src="../assets/uncomplete.png" alt="uncomplete">
+            </li>
+          </ul>
+        </div>
+
+        <div class="completed-tasks">
+          <div class="completed-header">
+            <p>Completed Tasks</p>
+          </div>
+          <ul id="tasks" class="tasks">
+            <li class="completed-animation" v-for="(completedTodo, index) in completedTodos" :key="completedTodo.id">
+              <p>{{ completedTodo.content }}</p>
+              <img @click="deleteTask(index)" src="../assets/complete.png" alt="uncomplete">
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
